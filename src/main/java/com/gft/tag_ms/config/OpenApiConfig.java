0df5b176642a9e8b1bb892e6626baff0b37330c3
Daniel Nascimento Tomaz <daniel.tomaz@gft.com>
2025-12-02 03:46:47 -0300

    feat(Swagger): Erro cors

--- src/main/java/com/gft/tag_ms/config/OpenApiConfig.java
+++ src/main/java/com/gft/tag_ms/config/OpenApiConfig.java
@@ -4,11 +4,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -38,13 +35,6 @@ public class OpenApiConfig {
                         .license(new License()
                                 .name("MIT License")
                                 .url("https://opensource.org/licenses/MIT"))
-                )
-
-                .servers(List.of(
-                        new Server()
-                                .url("http://localhost:8080")
-                                .description("Servidor local de desenvolvimento")
-                )
                 );
 
 
